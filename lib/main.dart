b18e06d5e2925a7df8bb7a0254a7ef87b12dfb90
star girl <aminaidris@icloud.com>
2026-07-28 01:16:15 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -34,22 +34,7 @@ Future<void> main() async {
   );
 
   WidgetsFlutterBinding.ensureInitialized();
-  if(GetPlatform.isAndroid) {
-    if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp(
-        options: const FirebaseOptions(
-          apiKey: "AIzaSyA37FqlmRP0eV54IiWkU7LTQTMkLAFt_Yw",
-          appId: "1:464484124395:android:0d3704934d42202e7520ae",
-          messagingSenderId: "464484124395",
-          projectId: "valencias-bestall-mat",
-        ),
-      );
-    }
-  } else {
-    if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp();
-    }
-  }
+  await Firebase.initializeApp();
 
   cameras = await availableCameras();
 
